d6de488a626626044468583bd745eb450a32c125
LuukHesselshva <luuk.hessels@hva.nl>
2025-05-15 13:56:11 +0200

    Add files via upload

---
+++ code model.py
@@ -0,0 +1,25 @@
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# start waarde
+massa = 1 # in kg
+demmpingsfactor = 1 # Kg/s
+veer_constante = 1 # N/m
+x0 = 0 # m
+v0 = 0 # m/s
+F0 = 0 # Ns
+
+# tijd(s)
+t0 = 0
+teind = 10
+dt = 0.01
+nt = int((teind - t0)/ dt)
+tijd = np.linspace(t0,teind,nt)
+
+#kritische demp
+b_krit = math.sqrt(4*massa*veer_constante)
+
+def model(t,m,b,k,x0,v0):
+    pass
